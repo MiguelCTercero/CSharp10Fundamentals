96f5664f98710970423f9173e440e854c8bc623b
Miguel Tercero <Miguel.C.Tercero@gmail.com>
2023-06-19 17:47:12 -0400

    End of Chapter 3

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,9 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string name = "Gill";
-string lastName = "Cleeren";
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-Console.WriteLine("Hello " + name + " " + lastName);
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
 
+int hoursWorked;
+hoursWorked = 125;
+hoursWorked = 148;
+//MonthlyWage = true;
+const double interestRate = 0.07;
+//interestRate = 0.08;
+
+string firstName = "Bethany";
+string lastName = "Smith";
+
+string emptyString = "";
 
 
