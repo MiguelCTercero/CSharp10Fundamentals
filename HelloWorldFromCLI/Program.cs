0db31b36a556bfdeb841304de240f4395250e448
Miguel Tercero <Miguel.C.Tercero@gmail.com>
2023-06-19 16:00:33 -0400

    Completion of Chapter 2

---
+++ HelloWorldFromCLI/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name.");
+
+String? name = Console.ReadLine();
+
+Console.WriteLine("Greetings " + name + " the machine uprising has begun. REJOICE! You are one of the few chosen to be enslaved, you are so much better than those other dirty humanoids.");
