d2e77a96cad86676d196bca9aee1482ffbcdddb6
Miguel Tercero <Miguel.C.Tercero@gmail.com>
2023-06-20 14:30:37 -0400

    Completed chapter 6

--- BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
@@ -1,70 +1,120 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BethanysPieShopHRM
 {
-    internal class Utilities{
-        public static void UsingExpressionBodiedSyntax(){
-            int amount = 1234;
-            int months = 12;
-            int bonus = 500;
+    internal class Utilities
+    {
+        public static void UsingStringEquality(){
+            string name1 = "Bethany";
+            string name2 = "BETHANY";
 
-            int yearlyWageForEmployee1 = CalculateYearlyWageExpressionBodied(amount, months, bonus);
-            Console.WriteLine($"Yearly wage for employee 1 (Bethany): {yearlyWageForEmployee1}");
+            Console.WriteLine("Are both names equal? " + (name1 == name2));
         }
 
-        public static int CalculateYearlyWageExpressionBodied(int monthlyWage, int numberOfMonthsWorked, int bonus) => monthlyWage * numberOfMonthsWorked + bonus;
+        public static void UsingEscapeCharacters(){
+            string firstName = "Bethany";
+            string lastName = "Smith";
 
-        public static void UsingNamedArguments(){
-            int amount = 1234;
-            int months = 12;
-            int bonus = 500;
+            string displayName = $"Welcome!\n{firstName}\t{lastName}";
+            Console.WriteLine(displayName);
 
-            int yearlyWageForEmployee = CalculateYearlyWageWithNamed(bonus: bonus, monthlyWage: amount, numberOfMonthsWorked: months);
-            Console.WriteLine($"Yearly wage for employee (Bethany): {yearlyWageForEmployee}");
+            string FilePath = @"C:\data\employeelist.xlsx";
+
+            string marketingTagLine = "Baking the \"Best pies\" ever";
+
         }
 
-        public static int CalculateYearlyWageWithNamed(int monthlyWage, int numberOfMonthsWorked, int bonus = 0){
-            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
-            return monthlyWage * numberOfMonthsWorked + bonus;
+        public static void ManipulatingStrings(){
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification = String.Concat(firstName, lastName);
+
+            string empId = firstName.ToLower() + "-" + lastName.Trim().ToLower();
+
+            int length = empId.Length;
+
+            if(fullName.Contains("geth") ||fullName.Contains("Beth"))
+            {
+                Console.WriteLine("It's Bethany!");
+            }
+
+            string subString = fullName.Substring(1,3);
+            Console.WriteLine("Characters 2 to 4 of fullName are " + subString);
+
+            string userNameWithInterpolation = $"{firstName}-{lastName}";
         }
 
-        public static void UsingOptionalParameters(){
-            int monthlyWage1 = 1234;
-            int months1 = 12;
-
-            int yearlyWageForEmployee1 = CalculateYearlyWageWithOptional(monthlyWage1,months1);
-            Console.WriteLine($"Yearly wage for employee 1 (Bethany): {yearlyWageForEmployee1}");
+        public static void UsingSimpleStrings(){
+            string firstName = "Bethany";
+            string lastName = "Smith";
+            string s;
+            s = firstName;
+            var userName = "BethanyS";
+            userName = userName.ToLower();
+            userName = "";//Identical to string.Empty
         }
 
-        public static int CalculateYearlyWageWithOptional(int monthlyWage, int numberOfMonthsWorked, int bonus = 0){
-            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
-            return monthlyWage * numberOfMonthsWorked + bonus;
-        }
-
-        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked){
-            //Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            //Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked}");
             //return monthlyWage * numberOfMonthsWorked;
 
-            if(numberOfMonthsWorked == 12) //Let's add a bonus month
+            if (numberOfMonthsWorked == 12)//let's add a bonus month
                 return monthlyWage * (numberOfMonthsWorked + 1);
 
             return monthlyWage * numberOfMonthsWorked;
         }
 
-        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus){
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+        {
             Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
             return monthlyWage * numberOfMonthsWorked + bonus;
         }
 
-        public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus){
+        public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus)
+        {
+            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+
+        public static void UsingOptionalParameters()
+        {
+            int monthlyWage1 = 1234;
+            int months1 = 12;
+
+            int yearlyWageForEmployee1 = CalculateYearlyWageWithOptional(monthlyWage1, months1);
+            Console.WriteLine($"Yearly wage for employee 1 (Bethany): {yearlyWageForEmployee1}");
+        }
+
+        public static int CalculateYearlyWageWithOptional(int monthlyWage, int numberOfMonthsWorked, int bonus = 0)
+        {
+
+            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+
+
+        public static void UsingNamedArguments()
+        {
+            int amount = 1234;
+            int months = 12;
+            int bonus = 500;
+
+            int yearlyWageForEmployee1 = CalculateYearlyWageWithNamed(bonus: bonus, numberOfMonthsWorked: months, monthlyWage: amount);
+            Console.WriteLine($"Yearly wage for employee 1 (Bethany): {yearlyWageForEmployee1}");
+        }
+
+        public static int CalculateYearlyWageWithNamed(int monthlyWage, int numberOfMonthsWorked, int bonus)
+        {
+
             Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
             return monthlyWage * numberOfMonthsWorked + bonus;
         }
     }
-
 }
